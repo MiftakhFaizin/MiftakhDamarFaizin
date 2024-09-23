edfa94423fcfd90fbbabd131ba503e9021e652e5
MiftakhFaizin <miftakhdamar07@gmail.com>
2024-09-23 21:09:52 +0700

    .

---
+++ script.js
@@ -0,0 +1,5 @@
+let btnUbahWarna = document.querySelector('.btn-ubah-warna')
+btnUbahWarna.addEventListener('click', ()=> {
+    console.log("clicked")
+    document.body.style.backgroundColor = "green"
+})
